3a6ea0c9b77bc798a6e0b70e398438c431cc4c51
ndi1 <jhelton773@gmail.com>
2022-01-25 09:53:46 -0500

    Continued implementing food services and pages

--- src/main/java/com/jh/shopperweb/controllers/FoodController.java
+++ src/main/java/com/jh/shopperweb/controllers/FoodController.java
@@ -1,7 +1,75 @@
 package com.jh.shopperweb.controllers;
 
+import com.jh.shopperweb.food.Food;
+import com.jh.shopperweb.food.FoodNotFoundException;
+import com.jh.shopperweb.food.FoodRepository;
+import com.jh.shopperweb.food.FoodService;
+import com.jh.shopperweb.user.User;
+import com.jh.shopperweb.user.UserNotFoundException;
+import com.jh.shopperweb.user.UserRepository;
+import com.jh.shopperweb.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 public class FoodController {
+    @Autowired private FoodService service;
+
+
+    @GetMapping("/myFoods")
+    public String showFoodList(Model model){
+        List<Food> listFoods = service.listAll();
+        model.addAttribute("listFoods",listFoods);
+        return "myFoods";
+    }
+
+
+    @GetMapping("/myFoods/new")
+    public String showNewForm(Model model){
+        model.addAttribute("food",new Food());
+        model.addAttribute("pageTitle","Add New Food");
+        return "user_form";
+    }
+
+    @PostMapping("/myFoods/save")
+    public String saveUser(Food food){
+        service.save(food);
+        return "redirect:/myFoods";
+    }
+
+    @GetMapping("/foods/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+        try {
+            Food food = service.get(id);
+            model.addAttribute("food", food);
+            model.addAttribute("pageTitle", "Edit Food (ID: " + id + ")");
+            return "addFoods";
+        } catch (FoodNotFoundException e) {
+            ra.addFlashAttribute("message", e.getMessage());
+            return "redirect:/myFoods";
+
+        }
+    }
+
+    @GetMapping("/myFoods/delete/{id}")
+    public String deleteFood(@PathVariable("id") Integer id, RedirectAttributes ra){
+        try {
+            service.delete(id);
+            ra.addFlashAttribute("message", "The Food ID " + id + " has been deleted.");
+
+        } catch (FoodNotFoundException e) {
+            ra.addFlashAttribute("message",e.getMessage());
+
+        }
+        return "redirect:/myFoods";
+    }
+
+
 }
--- src/main/java/com/jh/shopperweb/food/FoodService.java
+++ src/main/java/com/jh/shopperweb/food/FoodService.java
@@ -2,10 +2,12 @@ package com.jh.shopperweb.food;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class FoodService {
     @Autowired private FoodRepository repo;
 
